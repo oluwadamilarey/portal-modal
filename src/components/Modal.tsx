4a6ab00b8e1a58110ffa84fa0bfffe564218bc35
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2021-11-28 12:36:03 +0100

    solved useRef typeErrors

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useRef, useCallback, useEffect } from "react";
+import { useSpring, animated } from "react-spring";
 import styled from "styled-components";
+import { MdClose } from "react-icons/md";
 
 const BackGround = styled.div`
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0);
+  background: rgba(0, 0, 0, 0.5);
   position: fixed;
   display: flex;
   justify-content: center;
@@ -18,7 +20,7 @@ const ModalWrapper = styled.div`
   background: #fff;
   color: #000;
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
   position: relative;
   z-index: 10;
   border-radius: 10px;
@@ -31,6 +33,37 @@ const ModalImg = styled.img`
   background: #000;
 `;
 
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
+
 export const Modal = ({
   showModal,
   setShowModal,
@@ -38,5 +71,41 @@ export const Modal = ({
   showModal: any;
   setShowModal: any;
 }) => {
-  return <>{showModal ? <div>Modal</div> : null}</>;
+  const ModalRef = useRef<HTMLDivElement>(null); //this ensures that ModalRef = HTMLDivElement | null which is what the ref prop is expecting rather than HTMLDivElement | undefined.
+
+  const animation = useSpring({
+    config: {
+      duration: 250,
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0%)` : `translateY(-100%)`,
+  });
+
+  const closeModal = (e: any) => {
+    if (ModalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  return (
+    <>
+      {showModal ? (
+        <BackGround ref={ModalRef} onClick={closeModal}>
+          <animated.div style={animation as any}>
+            <ModalWrapper>
+              <ModalContent>
+                <h1>Are you ready?</h1>
+                <p>Get Exvlusive Access to our next Lauch?</p>
+                <button>Join Now!</button>
+              </ModalContent>
+              <CloseModalButton
+                aria-label="Close Modal"
+                onClick={() => setShowModal((prev: Boolean) => !prev)}
+              />
+            </ModalWrapper>
+          </animated.div>
+        </BackGround>
+      ) : null}
+    </>
+  );
 };
